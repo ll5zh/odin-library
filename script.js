60706ac015c7b227556b625947c42f5cb36ace1b
Lucia <122565245+ll5zh@users.noreply.github.com>
2023-08-10 12:01:22 -0400

    Add function to display library

--- script.js
+++ script.js
@@ -1,16 +1,37 @@
+// HTML elements
+const table = document.querySelector('table');
+
 // constructing book objects for the library
 
 let myLibrary = [];
 
-function Book(title, author, pageCount, isRead) {
+function Book(title, author, pages, isRead) {
     this.title = title;
     this.author = author;
-    this.pageCount = pageCount;
+    this.pages = pages;
     this.isRead = isRead;
 }
 
 function addBookToLibrary(book) {
     myLibrary.push(book);
+}
+
+function displayLibrary() {
+    for (let book of myLibrary) {
+        let row = table.insertRow(-1);
+        let title = row.insertCell(0);
+        let author = row.insertCell(1);
+        let pages = row.insertCell(2);
+        let isRead = row.insertCell(3);
+        title.innerHTML = book.title;
+        author.innerHTML = book.author;
+        pages.innerHTML = book.pages;
+        if (book.isRead) {
+            isRead.innerHTML = 'Read';
+        } else {
+            isRead.innerHTML = 'Not Read';
+        }
+    }
 }
 
 
@@ -21,3 +42,4 @@ console.log(Book);
 console.log(tml);
 addBookToLibrary(tml);
 console.log(myLibrary);
+displayLibrary();
